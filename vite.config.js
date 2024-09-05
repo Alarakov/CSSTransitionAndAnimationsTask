828a0df53e1e0b0eefc50a4cfb701dbb874c7f0e
Rodion <alarakov@gmail.com>
2024-09-05 22:20:09 +0300

    gg

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/CSSTransitionAndAnimationsTask/",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".css")) {
+            return "[name][extname]";
+          }
+          return "assets/[name][extname]";
+        },
+      },
+    },
+  },
 });
