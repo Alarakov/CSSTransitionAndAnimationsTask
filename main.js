6eb2ddf94f2d55e9398bfc5f35da1ca24a0743b8
Rodion <alarakov@gmail.com>
2024-09-05 21:16:28 +0300

    firstTask

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const character = document.querySelector(".character");
+  document.addEventListener("keyup", (event) => {
+    if (event.code === "Space" && !character.classList.contains("jump")) {
+      character.classList.add("jump");
+    }
+  });
+
+  character.addEventListener("animationend", (event) => {
+    if (event.animationName === "jump") {
+      character.classList.remove("jump");
+    }
+  });
+});
